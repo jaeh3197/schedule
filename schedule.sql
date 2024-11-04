b01652370880828d64399598ba81457d3b722d48
jaeh3197 <jaeh3197@naver.com>
2024-11-04 11:04:01 +0900

    feat : ERD 수정 및 sql 추가
    ERD 파일을 수정하고 sql 파일을 완성하였습니다.

--- schedule.sql
+++ schedule.sql
@@ -1,12 +1,33 @@
 -- schedule 테이블 생성
 CREATE TABLE `schedule` (
-                            `ID`	LONG	NOT NULL,
-                            `NAME`	STRING	NULL,
-                            `PASSWORD`	lONG	NULL,
-                            `TITLE`	STRING	NULL,
-                            `CONTENT`	STRING	NULL,
-                            `CREATED_AT`	DATE	NULL,
-                            `MODIFIED_AT`	DATE	NULL
+                `id`	INT	AUTO_INCREMENT,
+                `name`	VARCHAR(10)	NOT NULL,
+                `password`	INT	NOT NULL,
+                `title`	VARCHAR(20)	NOT NULL,
+                `content`	VARCHAR(40)	NOT NULL,
+                `created_at`	DATE NOT NULL,
+                `modified_at`	DATE NOT NULL,
+                PRIMARY KEY(id)
 );
 
--- 
+-- 전체 목록 조회
+SELECT *
+FROM schedule;
+
+-- 선택 일정 조회
+SELECT *
+FROM schedule
+WHERE id = 1;
+
+-- 일정 생성
+INSERT INTO schedule (name,password,title,content,created_at,modified_at)
+VALUES ('김철수',1234,'운동','수영','2024-11-04','2024-11-04')
+
+-- 선택한 일정 수정
+UPDATE schedule
+SET content = '수정된 내용'
+WHERE id = 1;
+
+-- 선택한 일정 삭제
+DELETE from schedule
+where id =1;
